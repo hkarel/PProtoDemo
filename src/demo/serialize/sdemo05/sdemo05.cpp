d17efbdc06c5b836093c33f377f5953f2b9ec358
Pavel Karelin <hkarel@yandex.ru>
2021-12-12 16:01:20 +0300

    Добавлена сериализация для множественного наследования (Demo 05)

--- src/demo/serialize/sdemo05/sdemo05.cpp
+++ src/demo/serialize/sdemo05/sdemo05.cpp
@@ -18,7 +18,14 @@ struct BaseStruct
 {
     qint8   value1 = {0};
     quint32 value2 = {0};
-    quint64 value3 = {0};
+
+    DECLARE_B_SERIALIZE_FUNC
+};
+
+struct BaseStruct2
+{
+    qint8  value3 = {0};
+    qint64 value4 = {0};
 
     DECLARE_B_SERIALIZE_FUNC
 };
@@ -28,10 +35,10 @@ struct BaseDeserializeDummy : BaseStruct
     DECLARE_B_SERIALIZE_FUNC
 };
 
-struct DerivedStruct : BaseStruct
+struct DerivedStruct : BaseStruct, BaseStruct2
 {
-    QString value4;
-    QUuidEx value5;
+    QString value5;
+    QUuidEx value6;
 
     DECLARE_B_SERIALIZE_FUNC
 };
@@ -41,7 +48,6 @@ bserial::RawVector BaseStruct::toRaw() const
     B_SERIALIZE_V1(stream)
     stream << value1;
     stream << value2;
-    stream << value3;
     B_SERIALIZE_RETURN
 }
 
@@ -50,7 +56,22 @@ void BaseStruct::fromRaw(const bserial::RawVector& vect)
     B_DESERIALIZE_V1(vect, stream)
     stream >> value1;
     stream >> value2;
+    B_DESERIALIZE_END
+}
+
+bserial::RawVector BaseStruct2::toRaw() const
+{
+    B_SERIALIZE_V1(stream)
+    stream << value3;
+    stream << value4;
+    B_SERIALIZE_RETURN
+}
+
+void BaseStruct2::fromRaw(const bserial::RawVector& vect)
+{
+    B_DESERIALIZE_V1(vect, stream)
     stream >> value3;
+    stream >> value4;
     B_DESERIALIZE_END
 }
 
@@ -72,8 +93,9 @@ bserial::RawVector DerivedStruct::toRaw() const
 {
     B_SERIALIZE_V1(stream)
     stream << B_BASE_CLASS(BaseStruct);
-    stream << value4;
+    stream << B_BASE_CLASS(BaseStruct2);
     stream << value5;
+    stream << value6;
     B_SERIALIZE_RETURN
 }
 
@@ -81,8 +103,9 @@ void DerivedStruct::fromRaw(const bserial::RawVector& vect)
 {
     B_DESERIALIZE_V1(vect, stream)
     stream >> B_BASE_CLASS(BaseStruct);
-    stream >> value4;
+    stream >> B_BASE_CLASS(BaseStruct2);
     stream >> value5;
+    stream >> value6;
     B_DESERIALIZE_END
 }
 
@@ -103,8 +126,9 @@ int main(int /*argc*/, char* /*argv*/[])
     data.value1 = 3;
     data.value2 = 4;
     data.value3 = 5;
-    data.value4 = "string data";
-    data.value5 = QUuid::createUuid();
+    data.value4 = 6;
+    data.value5 = "string data";
+    data.value6 = QUuid::createUuid();
 
     log_info << "---";
     log_info << "Source data";
@@ -113,6 +137,7 @@ int main(int /*argc*/, char* /*argv*/[])
     log_info << "Value3: " << data.value3;
     log_info << "Value4: " << data.value4;
     log_info << "Value5: " << data.value5;
+    log_info << "Value6: " << data.value6;
 
     { //Block for QDataStream
         QDataStream stream {&buff, QIODevice::WriteOnly};
@@ -120,34 +145,34 @@ int main(int /*argc*/, char* /*argv*/[])
         stream << data;
     }
 
-    BaseStruct data2;
+    BaseStruct baseData;
     { //Block for QDataStream
         QDataStream stream {buff};
         STREAM_INIT(stream);
 
         // Для извлечения из потока только базового класса потребуется
         // вспомогательная структура с фиктивной функцией десериализации
-        stream >> *((BaseDeserializeDummy*)&data2);
+        stream >> *((BaseDeserializeDummy*)&baseData);
     }
     log_info << "---";
     log_info << "Deserialized base data";
-    log_info << "Value1: " << data2.value1;
-    log_info << "Value2: " << data2.value2;
-    log_info << "Value3: " << data2.value3;
+    log_info << "Value1: " << baseData.value1;
+    log_info << "Value2: " << baseData.value2;
 
-    DerivedStruct data3;
+    DerivedStruct dataDerived;
     { //Block for QDataStream
         QDataStream stream {buff};
         STREAM_INIT(stream);
-        stream >> data3;
+        stream >> dataDerived;
     }
     log_info << "---";
     log_info << "Deserialized derived data";
-    log_info << "Value1: " << data3.value1;
-    log_info << "Value2: " << data3.value2;
-    log_info << "Value3: " << data3.value3;
-    log_info << "Value4: " << data3.value4;
-    log_info << "Value5: " << data3.value5;
+    log_info << "Value1: " << dataDerived.value1;
+    log_info << "Value2: " << dataDerived.value2;
+    log_info << "Value3: " << dataDerived.value3;
+    log_info << "Value4: " << dataDerived.value4;
+    log_info << "Value5: " << dataDerived.value5;
+    log_info << "Value6: " << dataDerived.value6;
 
     alog::stop();
     return 0;
